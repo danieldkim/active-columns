3bb89f632061e1a115a137984f459978d455c939
Daniel Kim <danieldkimster@gmail.com>
2010-11-26 23:16:55 -0500

    update tests to node-async-testing version 0.4.0

--- test/test-create_mem_object.js
+++ test/test-create_mem_object.js
@@ -3,7 +3,6 @@ require.paths.unshift('../lib');
 
 var sys = require('sys');
 var _ = require('underscore')._;
-var assert = require('assert');
 
 var log4js = require('log4js-node');
 log4js.addAppender(log4js.consoleAppender());
@@ -36,36 +35,36 @@ var suite = wrap({
     done();
   },
   suite: {
-    "No supercolumn, no column names": function(assert) {
-      _test_create_mem_object(assert, null, false)  
+    "No supercolumn, no column names": function(test) {
+      _test_create_mem_object(test, null, false)  
     },
 
-    "No supercolumn, no column names, json value type": function(assert) {
-      _test_create_mem_object(assert, null, false, 'json')  
+    "No supercolumn, no column names, json value type": function(test) {
+      _test_create_mem_object(test, null, false, 'json')  
     },
 
-    "No supercolumn, fixed column names": function(assert) {
-      _test_create_mem_object(assert, null, true)  
+    "No supercolumn, fixed column names": function(test) {
+      _test_create_mem_object(test, null, true)  
     },
 
-    "No supercolumn, fixed column names, json value type": function(assert) {
-      _test_create_mem_object(assert, null, true, 'json')
+    "No supercolumn, fixed column names, json value type": function(test) {
+      _test_create_mem_object(test, null, true, 'json')
     },
 
-    "Supercolumn, no column names": function(assert) {
-      _test_create_mem_object(assert, "test_super_column", false)  
+    "Supercolumn, no column names": function(test) {
+      _test_create_mem_object(test, "test_super_column", false)  
     },
 
-    "Supercolumn, no column names, json value type": function(assert) {
-      _test_create_mem_object(assert, "test_super_column", false, 'json')  
+    "Supercolumn, no column names, json value type": function(test) {
+      _test_create_mem_object(test, "test_super_column", false, 'json')  
     },
 
-    "Supercolumn, fixed column names": function(assert) {
-      _test_create_mem_object(assert, "test_super_column", true)  
+    "Supercolumn, fixed column names": function(test) {
+      _test_create_mem_object(test, "test_super_column", true)  
     },
 
-    "Supercolumn, fixed column names, json value type": function(assert) {
-      _test_create_mem_object(assert, "test_super_column", true, 'json')
+    "Supercolumn, fixed column names, json value type": function(test) {
+      _test_create_mem_object(test, "test_super_column", true, 'json')
     }
   },
   suiteTeardown: function(done) {
@@ -75,7 +74,7 @@ var suite = wrap({
 
 module.exports = { "create_mem_object() tests": suite };
 
-function _test_create_mem_object(assert, name, fixed_names, column_value_type) {
+function _test_create_mem_object(test, name, fixed_names, column_value_type) {
   var cf = {}
   if (name) cf.column_type = "Super"
   cf[(name ? 'sub' : '') + 'column_names'] = fixed_names ? ["col_1", "col_2"] : undefined,
@@ -92,43 +91,44 @@ function _test_create_mem_object(assert, name, fixed_names, column_value_type) {
 // sys.puts(" --- cf: " + JSON.stringify(cf))  
 // sys.puts(" --- o: " + JSON.stringify(o))  
   if (name) {
-   assert.equal(name, o.name)
+   test.equal(name, o.name)
   } else {
-   assert.equal("test_key", o.key)
+   test.equal("test_key", o.key)
   }
   if (fixed_names) {
     if (column_value_type && column_value_type == 'json') {
-      assert.equal(1, o.timestamps.col_1);
-      assert.equal(2, o.timestamps.col_2);
-      assert.equal("col_1", o.col_1.name);
-      assert.equal("1/a", o.col_1.col_a);
-      assert.equal("1/b", o.col_1.col_b);
-      assert.equal("col_2", o.col_2.name);
-      assert.equal("2/a", o.col_2.col_a);
-      assert.equal("2/b", o.col_2.col_b);       
+      test.equal(1, o.timestamps.col_1);
+      test.equal(2, o.timestamps.col_2);
+      test.equal("col_1", o.col_1.name);
+      test.equal("1/a", o.col_1.col_a);
+      test.equal("1/b", o.col_1.col_b);
+      test.equal("col_2", o.col_2.name);
+      test.equal("2/a", o.col_2.col_a);
+      test.equal("2/b", o.col_2.col_b);       
     } else {
-      assert.equal(1, o.timestamps.col_1);
-      assert.equal(2, o.timestamps.col_2);
-      assert.equal(val_1, o.col_1);
-      assert.equal(val_2, o.col_2);       
+      test.equal(1, o.timestamps.col_1);
+      test.equal(2, o.timestamps.col_2);
+      test.equal(val_1, o.col_1);
+      test.equal(val_2, o.col_2);       
     }
   } else {
     if (column_value_type && column_value_type == 'json') {
-      assert.equal("col_1", o.columns[0].name);
-      assert.equal("col_1", o.columns[0].name);
-      assert.equal("1/a", o.columns[0].col_a);
-      assert.equal("1/b", o.columns[0].col_b);
-      assert.equal(1, o.columns[0].timestamp);
-      assert.equal("col_2", o.columns[1].name);
-      assert.equal("col_2", o.columns[1].name);
-      assert.equal("2/a", o.columns[1].col_a);
-      assert.equal("2/b", o.columns[1].col_b);                   
-      assert.equal(2, o.columns[1].timestamp);
+      test.equal("col_1", o.columns[0].name);
+      test.equal("col_1", o.columns[0].name);
+      test.equal("1/a", o.columns[0].col_a);
+      test.equal("1/b", o.columns[0].col_b);
+      test.equal(1, o.columns[0].timestamp);
+      test.equal("col_2", o.columns[1].name);
+      test.equal("col_2", o.columns[1].name);
+      test.equal("2/a", o.columns[1].col_a);
+      test.equal("2/b", o.columns[1].col_b);                   
+      test.equal(2, o.columns[1].timestamp);
     } else {
-      assert.equal(val_1, o.columns[0].value);
-      assert.equal(val_2, o.columns[1].value);
+      test.equal(val_1, o.columns[0].value);
+      test.equal(val_2, o.columns[1].value);
     }
   }
 
+  test.finish();
 }
 
